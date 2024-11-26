776c3ac9804c6ae682c956981623c176ecd6859b
Benjos <benjamin@bourge.be>
2024-11-26 15:52:44 +0000

    [+] Plot the systems

--- ga.py
+++ ga.py
@@ -13,6 +13,8 @@ class Individual(NeuralNet):
         NeuralNet.__init__(self, layers)
         self.chromosome = self.set_random_chromosome(self.get_length_chromosome(layers))
         self.fitness = float('inf')
+        self.killed = False
+        self.gifted = False
 
     def set_random_chromosome(self, length):
         chromosome = []
@@ -27,9 +29,8 @@ class Individual(NeuralNet):
             l += layers[i+1]
         return l
 
-
 class GeneticAlgorithms:
-    def __init__(self, X, y, layers, population_size=100):
+    def __init__(self, X, y, layers, population_size=100, mode=0, nb_parthenogenesis=0, store_values=False):
         self._population = []
         for i in range(population_size):
             self._population.append(Individual(layers))
@@ -37,37 +38,109 @@ class GeneticAlgorithms:
         self._X = X
         self._y = y
         self._layers = layers
+        self._death_mode = mode
+        self._nb_parthenogenesis = nb_parthenogenesis
 
         self._bestIndividual = Individual(layers)
         self._bestChromosome = self._population[0].get_wb_as_1D()
         self._bestScore = float('inf')
 
         self._bestScoreThisIt = float('inf')
+        self._meanScoreThisIt = float('inf')
 
-    def choose_parents(self):
+        self._store_values = store_values
+        self._means = []
+        self._bests = []
+
+    def evaluatePopulation(self):
+        mean = 0
+        self._bestScoreThisIt = float('inf')
         for i in range(len(self._population)):
+            self._population[i].gifted = False
             yy = self._population[i].forward_propagation(self._X)[-1]
             fit = self._population[i].MSE(self._y, yy)
             self._population[i].fitness = fit
+            mean += fit
             if fit < self._bestScore:
                 self._bestChromosome = self._population[i].get_wb_as_1D()
                 self._bestScore = fit
             if fit < self._bestScoreThisIt:
                 self._bestScoreThisIt = fit
+        mean /= len(self._population)
+        self._meanScoreThisIt = mean
 
+    # ------- kill and gift selection ------
+
+    def worst_to_die(self, to_kill):
+        killed = []
+        for i in range(to_kill):
+            lowest = 0
+            lowestValue = float('inf')
+            for j in range(len(self._population)):
+                if self._population[j].killed:
+                    continue
+                if self._population[j].fitness < lowestValue:
+                    lowest = j
+                    lowestValue = self._population[j].fitness
+            self._population[lowest].killed = True
+            killed.append(lowest)
+        return killed
+
+    def tournament_to_die(self, to_kill):
+        killed = []
+        np.random.shuffle(self._population)
+        nb_per_batch = int(len(self._population) / (to_kill))
+        for i in range(to_kill):
+            lowest = i * nb_per_batch
+            for j in range(nb_per_batch):
+                if self._population[i * nb_per_batch + j].fitness < self._population[lowest].fitness:
+                    lowest = i * nb_per_batch + j
+            killed.append(lowest)
+        return killed
+
+    def best_to_gift(self, to_gift):
+        gifted = []
+        for i in range(to_gift):
+            highest = 0
+            highestValue = float('-inf')
+            for j in range(len(self._population)):
+                if self._population[j].killed or self._population[j].gifted:
+                    continue
+                if self._population[j].fitness > highestValue:
+                    highest = j
+                    highestValue = self._population[j].fitness
+            self._population[highest].gifted = True
+            gifted.append(highest)
+        return gifted
+
+    # --------------------------------------
+
+    def kill_and_gift(self, to_kill, to_gift):
+        np.random.shuffle(self._population) # allow random
+        killed = []
+        if self._death_mode == 0:
+            killed = self.worst_to_die(to_kill)
+        else:
+            killed = self.tournament_to_die(to_kill)
+        gifted =  self.best_to_gift(to_gift)
+
+        return killed, gifted
+
+    def choose_parents(self, nb_killed):
         np.random.shuffle(self._population)
         parents = []
-        for i in range(int(len(self._population) / 4)):
-            biggest = self._population[i * 4]
-            for j in range(4):
-                if self._population[i * 4 + j].fitness < biggest.fitness:
-                    biggest = self._population[i * 4 + j]
+        nb_per_batch = int(len(self._population) / (nb_killed * 2))
+        for i in range(nb_killed * 2):
+            biggest = self._population[i * nb_per_batch]
+            for j in range(nb_per_batch):
+                if self._population[i * nb_per_batch + j].fitness > biggest.fitness:
+                    biggest = self._population[i * nb_per_batch + j]
             parents.append(biggest)
         return parents
 
-    def mate(self, parents):
+    def mate(self, parents, killed):
         newborns = []
-        for i in range(int(len(parents) / 2)):
+        for i in range(len(killed)):
             new_chromosome = []
             for j in range(len(parents[i * 2].chromosome)):
                 r = np.random.randint(0, 2)
@@ -76,38 +149,38 @@ class GeneticAlgorithms:
                 else:
                     new_chromosome.append(parents[i * 2 + 1].chromosome[j])
             newborns.append(new_chromosome)
-        return newborns
+
+        for i in range(len(killed)):
+            self._population[killed[i]].killed = False
+            self._population[killed[i]].set_wb_from_1D(newborns[i])
+
+    def parthenogenesis(self, nb, gifted, killed):
+        for i in range(nb):
+            self._population[killed[i]].set_wb_from_1D(self._population[gifted[i]].get_wb_as_1D())
+            self._population[killed[i]].killed = False
 
     def mutate(self):
         nb_mutation = 80
-        mutation_strength = 0.5
+        mutation_strength = 0.2
         for i in range(len(self._population)):
+            if self._population[i].killed:
+                continue
             for j in range(nb_mutation):
                 random_index = random.randint(0, len(self._population[i].chromosome) - 1)
                 self._population[i].chromosome[random_index] += random.uniform(-1, 1) * mutation_strength
 
-    def kill(self, newborns):
-        # replace killed by newborns
-        np.random.shuffle(self._population)
-        killed = []
-        for i in range(int(len(self._population) / 8)):
-            lowest = i * 8
-            lowestValue = float('inf')
-            for j in range(8):
-                if self._population[i * 8 + j].fitness < lowestValue:
-                    lowest = i * 8 + j
-            killed.append(lowest)
-
-        for i in range(len(killed)):
-            self._population[killed[i]].set_wb_from_1D(newborns[i])
-
     def iterate(self, nb_iter=100):
         for i in range(nb_iter):
-            self._bestScoreThisIt = float('inf')
+            self.evaluatePopulation()
+            killed, gifted = self.kill_and_gift(15, self._nb_parthenogenesis)
+            self.parthenogenesis(self._nb_parthenogenesis, gifted, killed)
+            del killed[:self._nb_parthenogenesis]
             self.mutate()
-            parents = self.choose_parents()
-            newborns = self.mate(parents)
-            self.kill(newborns)
+            parents = self.choose_parents(len(killed))
+            self.mate(parents, killed)
+            if self._store_values:
+                self._means.append(self._meanScoreThisIt)
+                self._bests.append(self._bestScoreThisIt)
         self._bestIndividual.set_wb_from_1D(self._bestChromosome)
 
     def get_bestIndividualResult(self, X):
@@ -118,3 +191,12 @@ class GeneticAlgorithms:
 
     def get_bestScoreThisIt(self):
         return self._bestScoreThisIt
+
+    def get_meanScoreThisIt(self):
+        return self._meanScoreThisIt
+
+    def getAllMeans(self):
+        return self._means
+
+    def getAllBests(self):
+        return self._bests
--- main.py
+++ main.py
@@ -28,6 +28,72 @@ def color_by_coordinates(value, color1, color2):
         return (r, g, b)
 
 
+def setup_screen(height, max_x, max_y, min_x, min_y, pixel_size, square_size, width):
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Pixel Array Display")
+    square_x, square_y = (width - square_size) // 2, (height - square_size) // 2
+    grid_width = square_size // pixel_size
+    grid_height = square_size // pixel_size
+    x_coords = np.linspace(min_x, max_x, grid_width)
+    y_coords = np.linspace(max_y, min_y, grid_height)
+    xx, yy = np.meshgrid(x_coords, y_coords)
+    pixels_bg = np.column_stack((xx.ravel(), yy.ravel()))
+    square_surface = pygame.Surface((square_size, square_size))
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont(None, 24)  # Font for displaying FPS
+    return clock, font, grid_height, grid_width, pixels_bg, screen, square_surface, square_x, square_y
+
+
+def draw_datasets(X, y, max_x, max_y, min_x, min_y, my_y, square_size, square_surface):
+    for p in range(X.shape[0]):
+        px, py = X[p]
+        py *= -1
+
+        px = ((px - min_x) / (max_x - min_x)) * square_size
+        py = ((py - min_y) / (max_y - min_y)) * square_size
+
+        color = (255, 255, 255)
+
+        if y[p] == 1:
+            if my_y[p] == 1:
+                color = (0, 255, 0)  # TP = GREEN
+            else:
+                color = (255, 0, 255)  # FN = PURPLE
+        else:
+            if my_y[p] == 1:
+                color = (255, 255, 0)  # FP = YELLOW
+            else:
+                color = (255, 0, 0)  # TN = RED
+        pygame.draw.circle(square_surface, color, (int(px), int(py)), 4)
+
+
+def get_confusion_matrix(y, yy):
+    TP = 0
+    FN = 0
+    FP = 0
+    TN = 0
+    for p in range(y.shape[0]):
+        if y[p] == 1:
+            if yy[p] == 1:
+                TP += 1
+            else:
+                FN += 1
+        else:
+            if yy[p] == 1:
+                FP += 1
+            else:
+                TN += 1
+    return TP, FN, FP, TN
+
+
+def draw_background(grid_height, grid_width, pixel_size, square_surface, pixels_bg_colors):
+    for gy in range(grid_height):
+        for gx in range(grid_width):
+            v = (pixels_bg_colors[gy * grid_width + gx])
+            color = color_by_coordinates(v, (230, 126, 34), (52, 152, 219))
+            pygame.draw.rect(square_surface, color, (gx * pixel_size, gy * pixel_size, pixel_size, pixel_size))
+
+
 def bpr():
     X, y = make_moons(n_samples=100, noise = 0.1, random_state=21)
     y = y.reshape((y.shape[0], 1)) #from a big array to a multiples little arrays
@@ -46,20 +112,14 @@ def bpr():
     max_x = 3
     min_y = -3
     max_y = 3
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Pixel Array Display")
-    square_x, square_y = (width - square_size) // 2, (height - square_size) // 2
-    grid_width = square_size // pixel_size
-    grid_height = square_size // pixel_size
-    x_coords = np.linspace(min_x, max_x, grid_width)
-    y_coords = np.linspace(max_y, min_y, grid_height)
-    xx, yy = np.meshgrid(x_coords, y_coords)
-    pixels_bg = np.column_stack((xx.ravel(), yy.ravel()))
-
-    square_surface = pygame.Surface((square_size, square_size))
-
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont(None, 24)  # Font for displaying FPS
+    clock, font, grid_height, grid_width, pixels_bg, screen, square_surface, square_x, square_y = setup_screen(height,
+                                                                                                               max_x,
+                                                                                                               max_y,
+                                                                                                               min_x,
+                                                                                                               min_y,
+                                                                                                               pixel_size,
+                                                                                                               square_size,
+                                                                                                               width)
 
     nn.iteration_training(1000)
 
@@ -73,35 +133,12 @@ def bpr():
 
         #nn.iteration_training()
         pixels_bg_colors = nn.forward_propagation(pixels_bg)[-1]
+        draw_background(grid_height, grid_width, pixel_size, square_surface, pixels_bg_colors)
 
-        for gy in range(grid_height):
-            for gx in range(grid_width):
-                v = (pixels_bg_colors[gy * grid_width + gx])
-                color = color_by_coordinates(v, (230, 126, 34), (52, 152, 219))
-                pygame.draw.rect(square_surface, color,(gx * pixel_size, gy * pixel_size, pixel_size, pixel_size))
 
         my_y = nn.forward_propagation(X)[-1] > 0.5
 
-        for p in range(X.shape[0]):
-            px, py = X[p]
-            py *= -1
-
-            px = ((px - min_x) / (max_x - min_x)) * square_size
-            py = ((py - min_y) / (max_y - min_y)) * square_size
-
-            color = (255, 255, 255)
-
-            if y[p] == 1:
-                if my_y[p] == 1:
-                    color = (0, 255, 0) #TP = GREEN
-                else:
-                    color = (255, 0, 255) #FN = PURPLE
-            else:
-                if my_y[p] == 1:
-                    color = (255, 255, 0)  # FP = YELLOW
-                else:
-                    color = (255, 0, 0)  # TN = RED
-            pygame.draw.circle(square_surface, color, (int(px), int(py)), 4)
+        draw_datasets(X, y, max_x, max_y, min_x, min_y, my_y, square_size, square_surface)
 
         screen.fill((30, 30, 30))
         screen.blit(square_surface, (square_x, square_y))
@@ -117,8 +154,8 @@ def bpr():
 
 
 def gna():
-    # X, y = make_moons(n_samples=100, noise=0.1, random_state=21)
-    X, y = make_circles(n_samples=100, noise=0.1, factor=0.5, random_state=21)
+    X, y = make_moons(n_samples=100, noise=0.1, random_state=21)
+    # X, y = make_circles(n_samples=100, noise=0.1, factor=0.5, random_state=21)
     y = y.reshape((y.shape[0], 1))  # from a big array to a multiples little arrays
 
     ga = GeneticAlgorithms(X, y, [2, 16, 16, 1])
@@ -132,20 +169,14 @@ def gna():
     max_x = 2
     min_y = -2
     max_y = 2
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Pixel Array Display")
-    square_x, square_y = (width - square_size) // 2, (height - square_size) // 2
-    grid_width = square_size // pixel_size
-    grid_height = square_size // pixel_size
-    x_coords = np.linspace(min_x, max_x, grid_width)
-    y_coords = np.linspace(max_y, min_y, grid_height)
-    xx, yy = np.meshgrid(x_coords, y_coords)
-    pixels_bg = np.column_stack((xx.ravel(), yy.ravel()))
-
-    square_surface = pygame.Surface((square_size, square_size))
-
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont(None, 24)  # Font for displaying FPS
+    clock, font, grid_height, grid_width, pixels_bg, screen, square_surface, square_x, square_y = setup_screen(height,
+                                                                                                               max_x,
+                                                                                                               max_y,
+                                                                                                               min_x,
+                                                                                                               min_y,
+                                                                                                               pixel_size,
+                                                                                                               square_size,
+                                                                                                               width)
 
     ga.iterate(100)
 
@@ -158,36 +189,13 @@ def gna():
         square_surface.fill((0, 0, 0))
 
         ga.iterate(1)
-        pixels_bg_colors = ga.get_bestIndividualResult(pixels_bg)[-1]
 
-        for gy in range(grid_height):
-            for gx in range(grid_width):
-                v = (pixels_bg_colors[gy * grid_width + gx])
-                color = color_by_coordinates(v, (230, 126, 34), (52, 152, 219))
-                pygame.draw.rect(square_surface, color, (gx * pixel_size, gy * pixel_size, pixel_size, pixel_size))
+        pixels_bg_colors = ga.get_bestIndividualResult(pixels_bg)[-1]
+        draw_background(grid_height, grid_width, pixel_size, square_surface, pixels_bg_colors)
 
         my_y = ga.get_bestIndividualResult(X)[-1] > 0.5
 
-        for p in range(X.shape[0]):
-            px, py = X[p]
-            py *= -1
-
-            px = ((px - min_x) / (max_x - min_x)) * square_size
-            py = ((py - min_y) / (max_y - min_y)) * square_size
-
-            color = (255, 255, 255)
-
-            if y[p] == 1:
-                if my_y[p] == 1:
-                    color = (0, 255, 0)  # TP = GREEN
-                else:
-                    color = (255, 0, 255)  # FN = PURPLE
-            else:
-                if my_y[p] == 1:
-                    color = (255, 255, 0)  # FP = YELLOW
-                else:
-                    color = (255, 0, 0)  # TN = RED
-            pygame.draw.circle(square_surface, color, (int(px), int(py)), 4)
+        draw_datasets(X, y, max_x, max_y, min_x, min_y, my_y, square_size, square_surface)
 
         screen.fill((30, 30, 30))
         screen.blit(square_surface, (square_x, square_y))
@@ -200,7 +208,10 @@ def gna():
         screen.blit(text, (80, 10))
 
         text = font.render(f"Best MSE this iteration: {ga.get_bestScoreThisIt()}", True, (255, 255, 255))
-        screen.blit(text, (80, 40))
+        screen.blit(text, (80, 30))
+
+        text = font.render(f"Mean MSE this iteration: {ga.get_meanScoreThisIt()}", True, (255, 255, 255))
+        screen.blit(text, (80, 50))
 
         pygame.display.flip()
         clock.tick(60)
@@ -208,8 +219,116 @@ def gna():
     pygame.quit()
 
 
+def gna_testing():
+    X, y = make_moons(n_samples=100, noise=0.1, random_state=21)
+    # X, y = make_circles(n_samples=100, noise=0.1, factor=0.5, random_state=21)
+    y = y.reshape((y.shape[0], 1))  # from a big array to a multiples little arrays
+
+    ga0 = GeneticAlgorithms(X, y, [2, 16, 16, 1], mode=0, nb_parthenogenesis=0, store_values=True)
+    ga0.iterate(1000)
+
+    my_y = ga0.get_bestIndividualResult(X)[-1] > 0.5
+
+    TP, FN, FP, TN = get_confusion_matrix(y, my_y)
+    print("No parthenogenesis, worst to die:")
+    print("TP: ", TP)
+    print("FN: ", FN)
+    print("FP: ", FP)
+    print("TN: ", TN)
+
+    print("best overall: ", ga0.get_bestScore())
+
+    # -------------------------
+
+    ga1 = GeneticAlgorithms(X, y, [2, 16, 16, 1], mode=1, nb_parthenogenesis=0, store_values=True)
+    ga1.iterate(1000)
+
+    my_y = ga1.get_bestIndividualResult(X)[-1] > 0.5
+
+    TP, FN, FP, TN = get_confusion_matrix(y, my_y)
+    print("No parthenogenesis, tournament to die:")
+    print("TP: ", TP)
+    print("FN: ", FN)
+    print("FP: ", FP)
+    print("TN: ", TN)
+
+    print("best overall: ", ga1.get_bestScore())
+
+    # -------------------------
+
+    ga2 = GeneticAlgorithms(X, y, [2, 16, 16, 1], mode=0, nb_parthenogenesis=5, store_values=True)
+    ga2.iterate(1000)
+
+    my_y = ga2.get_bestIndividualResult(X)[-1] > 0.5
+
+    TP, FN, FP, TN = get_confusion_matrix(y, my_y)
+    print("parthenogenesis, worst to die:")
+    print("TP: ", TP)
+    print("FN: ", FN)
+    print("FP: ", FP)
+    print("TN: ", TN)
+
+    print("best overall: ", ga2.get_bestScore())
+
+    # -------------------------
+
+    ga3 = GeneticAlgorithms(X, y, [2, 16, 16, 1], mode=1, nb_parthenogenesis=5, store_values=True)
+    ga3.iterate(1000)
+
+    my_y = ga3.get_bestIndividualResult(X)[-1] > 0.5
+
+    TP, FN, FP, TN = get_confusion_matrix(y, my_y)
+    print("parthenogenesis, tournament to die:")
+    print("TP: ", TP)
+    print("FN: ", FN)
+    print("FP: ", FP)
+    print("TN: ", TN)
+
+    print("best overall: ", ga3.get_bestScore())
+
+    mean0 = ga0.getAllMeans()
+    best0 = ga0.getAllBests()
+    mean1 = ga1.getAllMeans()
+    best1 = ga1.getAllBests()
+    mean2 = ga2.getAllMeans()
+    best2 = ga2.getAllBests()
+    mean3 = ga3.getAllMeans()
+    best3 = ga3.getAllBests()
+
+    cmap = plt.get_cmap('viridis')  # Get the 'viridis' colormap
+    colors = cmap(np.linspace(0, 1, 8))  # Generate 8 colors evenly spaced
+
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+
+    x = np.linspace(0, 10, 1000)  # x-axis values
+
+    plt.plot(x, mean0, label=f'Line Mean Elitism', color=colors[0])
+    plt.plot(x, best0, label=f'Line Best Elitism', color=colors[1])
+    plt.plot(x, mean1, label=f'Line Mean 1', color=colors[2])
+    plt.plot(x, best1, label=f'Line Best 1', color=colors[3])
+    plt.plot(x, mean2, label=f'Line Mean 2', color=colors[4])
+    plt.plot(x, best2, label=f'Line Best 2', color=colors[5])
+    plt.plot(x, mean3, label=f'Line Mean 3', color=colors[6])
+    plt.plot(x, best3, label=f'Line Best 3', color=colors[7])
+
+    # Add labels, legend, and title
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Plot of 8 Arrays with Different Colors')
+    plt.legend()
+    plt.grid()
+
+    # Show the plot
+    plt.show()
+
+
+
+
+
+
 def main():
-    gna()
+    gna_testing()
 
 
 if __name__ == '__main__':
